2a09259598ce8d2e5d2178d809d17f7575749257
Eg2023-aliss <eg2023125@inti.edu.sv>
2025-10-20 10:36:40 -0600

    Update FPDF library path in ax_index.php

--- ax_index.php
+++ ax_index.php
@@ -4,7 +4,7 @@ ob_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require('fpdf/fpdf.php');
+require('expointi2025/fpdf/fpdf.php');
 
 // 🔐 Verificar sesión
 if (!isset($_SESSION['usuario_id'])) {
