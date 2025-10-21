12206349baf6b7ad1edd4df7a9fb53694fe8dff4
Eg2023-aliss <eg2023125@inti.edu.sv>
2025-10-20 18:52:11 -0600

    Update FPDF require statement in empleados_index.php

--- empleados_index.php
+++ empleados_index.php
@@ -4,7 +4,7 @@ ob_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require('fpdf/fpdf_index.php');
+require('fpdf/fpdf.php');
 
 // 🔐 Verificar sesión
 if (!isset($_SESSION['usuario_id'])) {
