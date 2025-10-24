3d17e1c780e5c4dfaa24beb2e11cba572a2bf15f
Eg2023-aliss <eg2023125@inti.edu.sv>
2025-10-24 09:02:04 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -12,7 +12,7 @@ function getPDO() {
 ];
         [
             'host' => 'aws-1-us-east-2.pooler.supabase.com',
-            'port' => '6543',
+            'port' => '5432',
             'dbname' => 'postgres',
             'user' => 'postgres.orzsdjjmyouhhxjfnemt',
             'pass' => 'Zv2sW23OhBVM5Tkz'
